766c8a438d2a906d722cc74eee405a690cf5c958
Giovani Cerejo Brasil <giovanicerejobrasil@gmail.com>
2023-10-31 20:22:49 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,9 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
+const keysCheck = document.querySelector(".keys-check input");
+
 let audio = new Audio("./src/tunes/a.wav");
+let mapedKeys = [];
 
 const playTune = (key) => {
   audio.src = `./src/tunes/${key}.wav`;
@@ -13,10 +17,27 @@ const playTune = (key) => {
   }, 150);
 };
 
+const handleVolume = (volume) => {
+  audio.volume = volume.target.value;
+};
+
+const showHideKeys = () => {
+  pianoKeys.forEach((key) => {
+    key.classList.toggle("hide");
+  });
+};
+
 pianoKeys.forEach((key) => {
   key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener("keydown", (event) => {
-  playTune(event.key);
+  if (mapedKeys.includes(event.key)) {
+    playTune(event.key);
+  }
 });
+
+volumeSlider.addEventListener("input", handleVolume);
+
+keysCheck.addEventListener("click", showHideKeys);
